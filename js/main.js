67f32396630730330bb4e47b91a36a5514b4d141
kelvinlau <kelvinlau14@gmail.com>
2017-05-02 00:41:04 -0700

    Final version of Project2

--- js/main.js
+++ js/main.js
@@ -1,69 +1,50 @@
-// Javascript file for Project 2
+$(function(){
 
-//User click on the menu
-//Select the section
-//Get the option value from the input
-//reload the page
-//Concatenate section value onto url
 
-//Send ajax request to NYT server
-//If Successful, append the 12 top new stories to the page
-// If not successful, display message 
-
-  
- $(function(){
+    $('.loader').hide()
 
     $('#section').change(function(){
 
-
       var topStoryItems = '';
       var homeSection = $('#section option').filter(':selected').val();  
-      var url = 'https://api.nytimes.com/svc/topstories/v2/' + homeSection+ '.json'; //Concatenation
-      console.log(url);
+      var url = 'https://api.nytimes.com/svc/topstories/v2/' + homeSection+ '.json'; 
           url += '?' + $.param({
           'api-key': '68f6f7797d58429bad830b72c33c7025'
         });
+      
+        $('.loader').show();
+        $('.site-header').addClass(".moveToTop");
+
+          $.ajax({
+            url: url,
+            method: 'GET',
+          }).done(function(data) {
+
+            $('.loader').hide()
+
+            var newYorkTimes = data.results;
+            var filteredList  = newYorkTimes.filter(function(item){return item.multimedia.length}).slice(0,12);
+
+              
+          $.each(filteredList, function(index,value){
+              topStoryItems += '<li class="article-list">';
+              topStoryItems += '<a href="' + value.url + '">'; 
+              topStoryItems += '<div class="innerStoryItems" style="background-image:url(' + value.multimedia[4].url + ')" >';
+              topStoryItems += '<p class="textbox">' + value.abstract + '</p> </div> </a>';
+              topStoryItems += '</li>';  
+          });
 
 
-        $.ajax({
-          url: url,
-          method: 'GET',
-        }).done(function(data) {
-
-
-        console.log(data);
-        console.log(data.results);
-
-        var newYorkTimes = data.results;
-
-          //   function checkingMultiMedia(item){
-          //     if(item.multimedia.length === 5) {
-          //       return true;
-          //   }
-          // }
-
-        var filteredList  = newYorkTimes.filter(function(item){return item.multimedia.length}).slice(0,12);
-            
-        console.log('hello, im the new object', filteredList);
-
-        $.each(filteredList, function(index,value){
-            topStoryItems += '<li>';
-            topStoryItems += '<img src ="' + value.multimedia[4].url + '" />';
-            topStoryItems += '<p>' + value.abstract + '</p>';
-            topStoryItems += '</li>';  
-        });
-
-        console.log(topStoryItems);
-
-        $('.top-story').empty();
-        $('.top-story').append(topStoryItems);
-          
-        }).fail(function() {
-          $('.top-story').append('Sorry, please try again!');
-          
-        }). always(function(){
-          // this code will run regardless of success or failure
-        });
+              $('.top-story').empty();
+              $('.top-story').append(topStoryItems);
+                
+              }).fail(function() {
+                $('.top-story').append('Sorry, please try again!');
+                $('.loader').hide();
+                
+              }). always(function(){
+                // this code will run regardless of success or failure
+              });
 
           
       });
